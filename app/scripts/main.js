25165b7eb2a74bae85aaba08f35e1a57b948c79f
Patrick Ring <ringpatrick89@gmail.com>
2017-10-10 21:10:51 +0200

    Add very basic animations

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -3,87 +3,124 @@
   'use strict';
 
   let scrollPosition = 0;
-  let windowHeight = 0;
-  let windowWidth = 0;
-  let lastScrollPos = 0;
+  let c01LastScrollPos = 0;
+  // let windowHeight = 0;
+  // let windowWidth = 0;
 
-  const $window = $('window');
-  const $body = $('body');
+  const $window = $(window);
+  // const $body = $('body');
   const requestAnimationFrame = window.requestAnimationFrame ||
                                 window.mozRequestAnimationFrame ||
                                 window.webkitRequestAnimationFrame ||
                                 window.oRequestAnimationFrame ||
                                 window.msRequestAnimationFrame;
+// ### page elements ### //
+  // const c01BigTitleChildren = $('.title-maplab').children();
+  let c01TitleMovement = false;
+  const c01BigTitleElements = {};
 
-  const c01BigTitleChildren = $('.title-maplab').children();
+// animation anchors
   const anchor = {
     a01: 750,
     a02: 720,
     a03: 3200
   };
 
+// ### functions ### //
+  function onScroll() {
+    requestAnimationFrame(parallax);
+    scrollPosition = $window.scrollTop();
+  }
+
   function parallax() {
     console.log('scrollPosition: ' + scrollPosition);
 
     moveTitle();
     moveTitleExtras();
-    // handleContainer02();
+  }
 
-    // ######## //
-
-    // $("#el").offset().top - $(document).scrollTop()
-
-    // yScroll = document.body.scrollTop || document.documentElement.scrollTop;
-    // yScroll = $(window).scrollTop();
-    // scrollPosition = window.scrollY;
-    // const stop = $('#P01-title-01').offset().top - ($(window).height() / 5);
-    // console.log(yScroll);
-    // console.log(stop)
-    /* const title01 = document.querySelectorAll('#P01-title-01'); */
-    /* $('#P01-title-06').css({
-      'transform': 'translate(' + (yScroll / 1.1) + '%, ' + yScroll + 'px)' // only move on x-axis
-    }); */
-
-    // ######## //
+  function setupTitleElements() {
+    const obj = c01BigTitleElements;
+    const children = $('.title-maplab').children();
+    children.each(i => {
+      const factor = (i === 1 || i === 4) ? 6 : (i === 2 || i === 5) ? 8 : 5;
+      const $el = children.eq(i);
+      const key = 'el' + i;
+      obj[key] = {
+        el: $el,
+        speed: factor,
+        lastPosition: 0
+      };
+    });
+    // console.log(c01BigTitleElements);
   }
 
   function moveTitle() {
+    // console.log('lastSP: ' + c01LastScrollPos);
+
     if (scrollPosition < anchor.a01) {
-      $(c01BigTitleChildren).each(i => {
-        const speed = (i === 1 || i === 4) ? 6 : (i === 2 || i === 5) ? 8 : 5;
-        const $el = c01BigTitleChildren.eq(i);
-        const y = scrollPosition / speed * -1;
+      // c01BigTitleChildren.each(i => {
+      //   const speed = (i === 1 || i === 4) ? 6 : (i === 2 || i === 5) ? 8 : 5;
+      //   const $el = c01BigTitleChildren.eq(i);
+      //   const y = scrollPosition / speed * -1;
 
-        setTranslate3d(0, y, 0, $el);
+      //   setTranslate3d(0, y, 0, $el);
+      // });
+
+      Object.keys(c01BigTitleElements).forEach(key => {
+        const y = scrollPosition / c01BigTitleElements[key].speed * -1;
+        setTranslate3d(0, y, 0, c01BigTitleElements[key].el);
       });
+      c01LastScrollPos = scrollPosition;
+      c01TitleMovement = true;
+    } else if (scrollPosition > 4900) {
+      Object.keys(c01BigTitleElements).forEach(key => {
+        const _key = c01BigTitleElements[key];
+        const _el = _key.el;
+
+        if (!c01TitleMovement) {
+          const d = getTranslateValues(_el);
+          _key.lastPosition = d.y; // Math.round(d.y);
+          console.log(_key.lastPosition);
+        }
+        const y = Math.round(((scrollPosition - 4900) - _key.lastPosition) * -1);
+        console.log('y: ' + y);
+
+        setTranslate3d(0, y, 0, _el);
+      });
+      c01TitleMovement = true;
+    } else {
+      c01TitleMovement = false;
     }
-    // lastScrollPos = scrollPosition;
-    // } else if (scrollPosition > 3200) {
-    //   // const pos = scrollPosition - lastScrollPos - testOffset - 400;
-    //   hh = true;
-    //   const pos = scrollPosition - lastScrollPos - $(window).height() - testOffset - 150;
-    //   console.log('lastScrollPos:' + lastScrollPos);
-    //   console.log('calcPos: ' + pos);
-
-    //   $(c01BigTitleChildren).each(i => {
-    //     const speed = (i === 1 || i === 4) ? 6 : (i === 2 || i === 5) ? 8 : 5;
-    //     // const speed = (i == 1 || i == 4) ? 190 : (i == 2 || i == 5) ? 180 : 200;
-
-    //     // console.log(children.eq(i));
-    //     c01BigTitleChildren.eq(i).css({
-    //       transform: 'translate(0px, ' + Math.round(pos / speed * -1) + 'px)'
-    //     });
-    //   });
-    // }
   }
 
   function moveTitleExtras() {
     const $el1 = $('.lab-verb-title-01');
     const $el2 = $('.lab-verb-title-02');
     const $el3 = $('.lab-verb-title-03');
+    // const $el4 = $('.x-scroll');
+    const $c03 = $('.c-03');
+    const $c04 = $('.c-04');
     const y1 = scrollPosition / 5;
     const y2 = scrollPosition * -1.2;
     const y3 = scrollPosition * -1.1;
+    const x4 = scrollPosition / 10;
+    const x5 = scrollPosition / 15;
+    const c03Top = $c03.offset().top;
+
+    const vTop = $window.scrollTop();
+    const vBottom = vTop + $window.height();
+    const c03Bottom = c03Top + $c03.outerHeight();
+
+    // console.log('vB : ' + vBottom);
+    // console.log('vT : ' + vTop);
+    // console.log('c03B : ' + c03Bottom);
+    // console.log('<c03T : ' + c03Top);
+    // vBottom < c03Top || vTop > c03Bottom
+
+    // console.log('top: ' + top);
+    // const bottom = top - $window.height();
+    // console.log('bottom: ' + bottom);
 
     // const $this = $('.lab-verb-title-02');
     // const offset = $this.offset().top;
@@ -107,12 +144,46 @@
     if (scrollPosition < 1100) {
       setTranslate3d(0, y2, 0, $el2);
     }
+
+    if (scrollPosition > 1400) {
+      const elD3 = getTranslateValues($el3);
+      const elD2 = getTranslateValues($el2);
+      setTranslate3d(-x5, elD3.y, 0, $el3);
+      setTranslate3d(x5, elD2.y, 0, $el2);
+    }
+    // if (scrollPosition > 1400) {
+    //   setTranslate3d(x4, 0, 0, $el4);
+    // }
+    // if (scrollPosition > $(document).height() - $c03.outerHeight()) {
+    //   setTranslate3d(x4, 0, 0, $c04);
+    // }
+    // if (!(vBottom < c03Top || vTop > c03Bottom)) {
+    //   setTranslate3d((x4 / 4), 0, 0, $c04);
+    // }
   }
 
-  function setTranslate3d(x, y, z, el) {
-    el.css({
+  function setTranslate3d(x, y, z, $el) {
+    $el.css({
       transform: 'translate3d(' + x + 'px, ' + y + 'px,' + z + 'px)'
     });
+  }
+
+  function getTranslateValues(_$el) {
+    const $el = _$el;
+    const transformMatrix = $el.css('-webkit-transform') ||
+                            $el.css('-moz-transform') ||
+                            $el.css('-ms-transform') ||
+                            $el.css('-o-transform') ||
+                            $el.css('transform');
+
+    const matrix = transformMatrix.replace(/[^0-9\-.,]/g, '').split(',');
+    // console.log(matrix);
+    const dx = parseInt(matrix[12] || matrix[4], 10);
+    const dy = parseInt(matrix[13] || matrix[5], 10);
+    // const dx = matrix[12] || matrix[4];
+    // const dy = matrix[13] || matrix[5];
+    // Math.round ??
+    return {x: dx, y: dy};
   }
 
   /*   $('html').on('mousewheel', event => {
@@ -148,17 +219,10 @@
       // $('.notPortrait').hide();
     }
   }
-
-  function onScroll() {
-    requestAnimationFrame(parallax);
-    scrollPosition = $(window).scrollTop();
-  }
-
+  setupTitleElements();
+  // window.addEventListener('onload', setup, false);
   window.addEventListener('orientationchange', testOrientation, false);
   window.addEventListener('scroll', onScroll, false);
-  // $window.on('scroll', () => {
-  //   onScroll();
-  // });
   // skrollr.init();
   // $(window).paroller();
 })();
